f05c922144cf22341e5ac215297f9283926e1f4c
David <davidlago104@gmail.com>
2022-01-15 22:01:12 -0500

    got a trash can icon to show up within innerhtml

--- index.js
+++ index.js
@@ -2,4 +2,16 @@ document.querySelector("#push").onclick = function (){
     if(document.querySelector("#newtask input").value.length == 0){
         alert("Nothing was added! You gotta do something!")
     }
+    else {
+        document.querySelector("#tasks").innerHTML += `
+        <div class="task">
+            <span id="taskname">
+                ${document.querySelector('#newtask input').value}
+            </span>
+            <button class="delete">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+            </button>
+        </div>
+        `
+    }
 }
